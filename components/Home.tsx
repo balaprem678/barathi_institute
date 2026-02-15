c36189afd1fe763fe082870d3a2150ef570f5efa
Prem <balaprem678@gmail.com>
2026-02-15 10:17:20 +0530

    changes

--- components/Home.tsx
+++ components/Home.tsx
@@ -563,17 +563,7 @@ export default function Home() {
       {/* FAQ Section */}
       <HomeFAQ />
 
-      <section className="call-out">
-        <div className="container">
-          <div className="content clearfix">
-            <div className="float_left">
-              <h2><strong>Want to be Join With Our Institution</strong></h2>
-            </div>
 
-            <div className="call-out-button" style={{ textAlign: 'right' }}><a className="thm-btn" href="https://forms.gle/9kCPJRg9aD3HKAmW9">Get Started with Your Application</a></div>
-          </div>
-        </div>
-      </section>
       <section className="why-chooseus sec-padd3">
         <div className="container">
           <div className="row">
